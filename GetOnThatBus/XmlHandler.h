586771dde74e6750f734bb048736b4249f26ca95
Glen Ruhl <intradinedev@gmail.com>
2016-03-20 20:11:28 -0400

    Organization, added file groups to proj file tree. Added model object XmlHandler (NSObject)

---
+++ GetOnThatBus/XmlHandler.h
@@ -0,0 +1,13 @@
+//
+//  XmlHandler.h
+//  GetOnThatBus
+//
+//  Created by Glen Ruhl on 3/20/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XmlHandler : NSObject
+
+@end
---
+++ GetOnThatBus/XmlHandler.m
@@ -0,0 +1,13 @@
+//
+//  XmlHandler.m
+//  GetOnThatBus
+//
+//  Created by Glen Ruhl on 3/20/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import "XmlHandler.h"
+
+@implementation XmlHandler
+
+@end
